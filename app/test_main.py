6d88c4f35bfd4d2ce8674a542dc7f8bf9d77bb38
bmv <m.barzii@mrt-m24.com>
2026-08-28 14:32:38 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,23 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_check_that_before_15_years_age_is_0() -> None:
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        pytest.param(14, 14, [0, 0], id="check_that_before_15_years_age_is_0"),
+        pytest.param(15, 15, [1, 1], id="check_that_15_years_age_is_1"),
+        pytest.param(23, 23, [1, 1], id="check_that_before_24_years_age_is_1"),
+        pytest.param(24, 24, [2, 2], id="check_that_24_years_age_is_2"),
+        pytest.param(100, 100, [21, 17], id="years_calculation_after_24"),
+        pytest.param(-24, -24, [0, 0], id="check_negative_int"),
+        pytest.param("24", "24", [0, 0], id="check_not_int"),
+    ]
+)
+def test_check_correct_calculating(
+        cat_age: int, dog_age: int, result: list
+) -> None:
     assert (
-        get_human_age(14, 14) == [0, 0]
-    ), "check_that_before_15_years_age_is_0"
-
-
-def test_check_that_15_years_age_is_1() -> None:
-    assert (
-        get_human_age(15, 15) == [1, 1]
-    ), "check_that_15_years_age_is_1"
-
-
-def test_check_that_before_24_years_age_is_1() -> None:
-    assert (
-        get_human_age(23, 23) == [1, 1]
-    ), "check_that_before_24_years_age_is_1"
-
-
-def test_check_that_24_years_age_is_2() -> None:
-    assert (
-        get_human_age(24, 24) == [2, 2]
-    ), "check_that_24_years_age_is_2"
-
-
-def test_years_calculation_after_24() -> None:
-    assert (
-        get_human_age(100, 100) == [21, 17]
-    ), "years_calculation_after_24"
+        get_human_age(cat_age, dog_age) == result
+    ), "calculating is not correct"
